56c7ba642c829c06b3600388595f15266e7805d8
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-18 11:53:32 +0530

    feature UC3

---
+++ MainUcThree.java
@@ -0,0 +1,17 @@
+
+
+
+class MainUcThree
+{
+    public static void main(String args[])
+    {
+          System.out.println(String.join(" ","    ****    ","   ****    ","  *********","    ******"));
+          System.out.println(String.join(" ","  ***  ***  ","  ***   ***","  *********","   **     "));
+          System.out.println(String.join(" ","  **     ** ","**      ** ","  *      **","  **      "));
+          System.out.println(String.join(" ","  **     ** ","**      ** ","  *********","    ****  "));
+          System.out.println(String.join(" ","  **     ** ","**      ** ","  *********","        **"));
+          System.out.println(String.join(" ","  ***  ***  ","  ***  *** ","  **       ","         **));
+          System.out.println(String.join(" ","    ****    ","   ****    ","  **       ","   ****** "));
+     }
+
+}
