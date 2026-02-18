559763376f081dd03b4b87111232e19874f74be3
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-18 11:27:12 +0530

    feature/UC2

---
+++ MainUcTwo.java
@@ -0,0 +1,46 @@
+/*
+
+@author : B.Sushma Sree
+@version: 2
+
+
+*/
+
+
+class Main
+{
+public static void main(String args[])
+{
+System.out.println("   ****    "+"  "+"   ****    "+"  *********"+"    ******");
+System.out.println("  ***  *** "+"  "+"  ***  *** "+"  *********"+"   **     ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *      **"+"  **      ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"    ****  ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"        **");
+System.out.println("  ***  *** "+"  "+"  ***  *** "+"  **       "+"         **);
+System.out.println("   ****    "+"  "+"   ****    "+"  **       "+"   ******");
+}
+
+}
+/*
+
+@author : B.Sushma Sree
+@version: 2
+
+
+*/
+
+
+class MainUcTwo
+{
+public static void main(String args[])
+{
+System.out.println("   ****    "+"  "+"   ****    "+"  *********"+"    ******");
+System.out.println("  ***  *** "+"  "+"  ***  *** "+"  *********"+"   **     ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *      **"+"  **      ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"    ****  ");
+System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"        **");
+System.out.println("  ***  *** "+"  "+"  ***  *** "+"  **       "+"         **);
+System.out.println("   ****    "+"  "+"   ****    "+"  **       "+"   ******");
+}
+
+}
